a133676431665dd05d3ae2ab602b5c129f807ddc
Alexander Kataev <sanekjackie@gmail.com>
2025-06-08 18:27:20 +0300

    otp

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import OTPInput from "@/ui/otp/OtpInput";
+import clsx from "clsx";
+import { useState } from "react";
+import styles from "./input.module.css";
+
+export default function Home() {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="bg-gray-800 h-[100vw] flex items-center justify-center">
+      <OTPInput
+        value={value}
+        onChange={(value) => {
+          setValue(value);
+          if (value.length === 4) {
+            alert(value);
+            setValue("");
+          }
+        }}
+        containerStyle={{ gap: "10px" }}
+        inputStyle={{ width: "50px", height: "50px" }}
+        renderInput={(qwe, index) => (
+          <input
+            {...qwe}
+            type="tel"
+            className={clsx(styles.input, qwe.value && styles["input-filled"])}
+          />
+        )}
+        shouldAutoFocus
+      />
+    </div>
+  );
+}
